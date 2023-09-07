6e6a80f09d4b204d98e6c52c85c2544e4a32ceeb
xshpg <xshgpxl@163.com>
2023-09-07 12:34:01 +0800

    测试一下

--- day02_1/05_function2/hi.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by xshgp on 2023/8/17.
-//
-
-#ifndef DAY02_1_HI_H
-#define DAY02_1_HI_H
-
-void sayHi();
-#endif //DAY02_1_HI_H
--- day02_1/06_header_impl/03_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "stu3.h"
-
-using namespace std;
-
-int main() {
-    cout << "age=" << age << endl;
-    return 0;
-}
